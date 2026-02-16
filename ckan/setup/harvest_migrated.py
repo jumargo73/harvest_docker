9b1f869ec1878a8ac13853b100e89b4080d0b9d7
jumargo73 <julianmarcelog@gmail.com>
2026-02-16 10:50:55 -0500

    my first commit

---
+++ ckan/setup/harvest_migrated.py
@@ -0,0 +1,12 @@
+from alembic.config import Config
+from alembic import command
+import os
+
+migration_path = "/srv/app/src/ckanext-harvest/ckanext/harvest/migration/harvest"
+
+cfg = Config(os.path.join(migration_path, "alembic.ini"))
+
+# PON TU URL REAL AQUÍ
+cfg.set_main_option("sqlalchemy.url", "postgresql://ckan_default:car2986@db/ckan_default")
+
+command.upgrade(cfg, "head")
